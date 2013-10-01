dcb9b37a0169747aa1800dd5619d5e3386ba3944
wgx-precy <wgx.nuaa@gmail.com>
2013-10-01 17:41:11 -0400

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'f4fae1df559277b843c5f0512c722fa985a962f07adac716dfd10b3d03e706ff44d6fd6a08a20c0db52a75ddcbdc20b2cbf7395e3692255671c018146a11404a'
