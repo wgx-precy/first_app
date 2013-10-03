766d029a2bc9e0aa9c1fb2006e1157932f50859e
wgx-precy <wgx.nuaa@gmail.com>
2013-10-03 16:30:02 -0400

    route path

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,11 @@ class StaticPagesController < ApplicationController
 
   def help
   end
+
+  def contact
+  end
+
+  def about
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 FirstApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
+  #get "users/new"
+
+  root  :to => 'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
   resources :microposts
 
   resources :users
